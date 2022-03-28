edd9a5568cfc79419e8ba899906fe0d276b5354c
TommyTran1 <tt01688@srcs.org>
2022-03-28 15:42:29 -0700

    Update FollowPlayer.cs

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -13,7 +13,7 @@ public class FollowPlayer : MonoBehaviour
     // Update is called once per frame
     public GameObject player;
     private Vector3 offset = new Vector3(0, 5, -7);
-    void Update()
+    void LateUpdate()
     {
         transform.position = player.transform.position + offset;
     }
